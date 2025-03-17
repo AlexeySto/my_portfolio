91d24635757733b0850c916a71d9f7b396f586d8
Alexey <stolyarov1986@inbox.ru>
2025-03-17 21:10:11 +0300

    Fixed bugs

--- src/components/SectionManager/SectionManager.js
+++ src/components/SectionManager/SectionManager.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 const SectionManager = () => {
   const [currentSection, setCurrentSection] = useState(0);
@@ -6,31 +7,51 @@ const SectionManager = () => {
   const [projectsClassName, setProjectsClassName] = useState('down');
   const [contactsClassName, setContactsClassName] = useState('down');
 
-  useEffect(() => {
-    const handleScroll = (event) => {
-      event.preventDefault();
+  const handleScroll = (event) => {
+    event.preventDefault();
 
-      const deltaY = event.deltaY;
+    const deltaY = event.deltaY;
 
-      if (deltaY > 0 && currentSection < 2) {
-        switch (currentSection) {
-          case 0:
-            setHeaderClassName('up');
-            setProjectsClassName('active');
-            setContactsClassName('down');
-            break;
-          case 1:
-            setHeaderClassName('up');
-            setProjectsClassName('up');
-            setContactsClassName('active');
-            break;
-          default:
-            break;
-        }
-        setCurrentSection((prev) => prev + 1);
+    if (deltaY > 0 && currentSection < 2) {
+      switch (currentSection) {
+        case 0:
+          setHeaderClassName('up');
+          setProjectsClassName('active');
+          setContactsClassName('down');
+          break;
+        case 1:
+          setHeaderClassName('up');
+          setProjectsClassName('up');
+          setContactsClassName('active');
+          break;
+        default:
+          break;
       }
+      setCurrentSection((prev) => prev + 1);
+    }
 
-      if (deltaY < 0 && currentSection > 0) {
+    if (deltaY < 0 && currentSection > 0) {
+      switch (currentSection) {
+        case 2:
+          setHeaderClassName('up');
+          setProjectsClassName('active');
+          setContactsClassName('down');
+          break;
+        case 1:
+          setHeaderClassName('active');
+          setProjectsClassName('down');
+          setContactsClassName('down');
+          break;
+        default:
+          break;
+      }
+      setCurrentSection((prev) => prev - 1);
+    }
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedUp: () => {
+      if (currentSection > 0) {
         switch (currentSection) {
           case 2:
             setHeaderClassName('up');
@@ -47,8 +68,31 @@ const SectionManager = () => {
         }
         setCurrentSection((prev) => prev - 1);
       }
-    };
+    },
+    onSwipedDown: () => {
+      if (currentSection < 2) {
+        switch (currentSection) {
+          case 0:
+            setHeaderClassName('up');
+            setProjectsClassName('active');
+            setContactsClassName('down');
+            break;
+          case 1:
+            setHeaderClassName('up');
+            setProjectsClassName('up');
+            setContactsClassName('active');
+            break;
+          default:
+            break;
+        }
+        setCurrentSection((prev) => prev + 1);
+      }
+    },
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
 
+  useEffect(() => {
     window.addEventListener('wheel', handleScroll, { passive: false });
 
     return () => {
@@ -60,6 +104,7 @@ const SectionManager = () => {
     headerClassName,
     projectsClassName,
     contactsClassName,
+    swipeHandlers,
   };
 };
 
