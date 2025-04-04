7b1717868490a818359ab94231da497ba8756baf
AlexeySto <stolyarov1986@inbox.ru>
2025-04-04 13:25:50 +0000

    Deploying to gh-pages from @ AlexeySto/my_portfolio@bebeb3c1250f722e70b5020502cac691bb3f86ea 🚀

---
+++ precache-manifest.0b84cc315827be6df3257812358736fd.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecc07f7100bbb40836ca4b094cb7adc",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "62924068f12d8fc92823",
+    "url": "/my_portfolio/static/css/main.eed4e215.chunk.css"
+  },
+  {
+    "revision": "131c85ce73ab9d55930b",
+    "url": "/my_portfolio/static/js/2.b9b7cbdb.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/my_portfolio/static/js/2.b9b7cbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46a7d96b7e3d592c3b7",
+    "url": "/my_portfolio/static/js/3.beb5a038.chunk.js"
+  },
+  {
+    "revision": "62924068f12d8fc92823",
+    "url": "/my_portfolio/static/js/main.6c8c3181.chunk.js"
+  },
+  {
+    "revision": "4c522ab8d28486b4e52d",
+    "url": "/my_portfolio/static/js/runtime-main.09da2cf5.js"
+  },
+  {
+    "revision": "ecd33d374ec2f84429161e627c224281",
+    "url": "/my_portfolio/static/media/Project-1-1.ecd33d37.webp"
+  },
+  {
+    "revision": "6ba24de4e7e81efd29c982d753db5ae5",
+    "url": "/my_portfolio/static/media/Project-1-2.6ba24de4.webp"
+  },
+  {
+    "revision": "5f48012560444a974870c3d65f7885e1",
+    "url": "/my_portfolio/static/media/Project-1-3.5f480125.webp"
+  },
+  {
+    "revision": "46ccd3e4b353ba20d6143d9cb336a4a8",
+    "url": "/my_portfolio/static/media/Project-1-4.46ccd3e4.webp"
+  },
+  {
+    "revision": "b5c051f2f0cf17024a564267565b888d",
+    "url": "/my_portfolio/static/media/Project-1-5.b5c051f2.webp"
+  },
+  {
+    "revision": "e4e01611d234281623655a72052eec85",
+    "url": "/my_portfolio/static/media/Project-1-6.e4e01611.webp"
+  },
+  {
+    "revision": "d5fbc1ae60b529b3fd2fb37062104403",
+    "url": "/my_portfolio/static/media/Project-1.d5fbc1ae.webp"
+  },
+  {
+    "revision": "1e49233298ccfd4ed511195fd6939199",
+    "url": "/my_portfolio/static/media/Project-2-1.1e492332.webp"
+  },
+  {
+    "revision": "d09925936d3ac535b3e419b5c815e882",
+    "url": "/my_portfolio/static/media/Project-2-2.d0992593.webp"
+  },
+  {
+    "revision": "509b83f500515ddc39282298f99efb62",
+    "url": "/my_portfolio/static/media/Project-2-3.509b83f5.webp"
+  },
+  {
+    "revision": "ffc88a4aef61e4888ade43282aa0f712",
+    "url": "/my_portfolio/static/media/Project-2.ffc88a4a.webp"
+  },
+  {
+    "revision": "2b99725c1ffd6152bb16bef3e87757a1",
+    "url": "/my_portfolio/static/media/Project-3-1.2b99725c.webp"
+  },
+  {
+    "revision": "1af380fae67577f2b47f5eeb245c8828",
+    "url": "/my_portfolio/static/media/Project-3-2.1af380fa.webp"
+  },
+  {
+    "revision": "b6062bfad4de95c57adc75a8bb14bca9",
+    "url": "/my_portfolio/static/media/Project-3.b6062bfa.webp"
+  },
+  {
+    "revision": "2856c8978f0ea89cb91634c88e6aa590",
+    "url": "/my_portfolio/static/media/Project-4-1.2856c897.webp"
+  },
+  {
+    "revision": "d5475d5102ff6143bdb402df67f4419d",
+    "url": "/my_portfolio/static/media/Project-4-2.d5475d51.webp"
+  },
+  {
+    "revision": "3f591516c23963b2faa04d0d4732a328",
+    "url": "/my_portfolio/static/media/Project-4.3f591516.webp"
+  },
+  {
+    "revision": "dc8fbaf6720ffe2b0b81a6c425fd22fd",
+    "url": "/my_portfolio/static/media/Project-5-1.dc8fbaf6.webp"
+  },
+  {
+    "revision": "eea1a96e9618cd60d41237ce485f606b",
+    "url": "/my_portfolio/static/media/Project-5-2.eea1a96e.webp"
+  },
+  {
+    "revision": "9b11e61be7faa570242d15c263cc9d9b",
+    "url": "/my_portfolio/static/media/Project-5.9b11e61b.webp"
+  },
+  {
+    "revision": "05f60e89f41241f5152c17a68165bfb1",
+    "url": "/my_portfolio/static/media/Project-6-1.05f60e89.webp"
+  },
+  {
+    "revision": "8c8e6f0cd89919eb845a7296c54a5b31",
+    "url": "/my_portfolio/static/media/Project-6-2.8c8e6f0c.webp"
+  },
+  {
+    "revision": "4ea7bc28a2a69b86f2e06fd5d33753ba",
+    "url": "/my_portfolio/static/media/Project-6-3.4ea7bc28.webp"
+  },
+  {
+    "revision": "e47f6b6279bb7fbbaaa19756adaf2363",
+    "url": "/my_portfolio/static/media/Project-6.e47f6b62.webp"
+  },
+  {
+    "revision": "efcc8de305077ecc830c57d8f8fed8fb",
+    "url": "/my_portfolio/static/media/background-dark.efcc8de3.webp"
+  },
+  {
+    "revision": "d01ee31ab46cb8d67c92da89947ed062",
+    "url": "/my_portfolio/static/media/background-white.d01ee31a.webp"
+  },
+  {
+    "revision": "a7643e6d2552d31222040fc3b408b2e4",
+    "url": "/my_portfolio/static/media/gitHub-black.a7643e6d.svg"
+  },
+  {
+    "revision": "c0e1ca3d6c5df5bd7dbad48c0f1d5cb2",
+    "url": "/my_portfolio/static/media/moon.c0e1ca3d.svg"
+  },
+  {
+    "revision": "cf7c05e70472a158e3c110bccd746eb8",
+    "url": "/my_portfolio/static/media/my-foto.cf7c05e7.webp"
+  },
+  {
+    "revision": "ccaa7d239372739f1894437fc06843a9",
+    "url": "/my_portfolio/static/media/sun.ccaa7d23.svg"
+  }
+]);
